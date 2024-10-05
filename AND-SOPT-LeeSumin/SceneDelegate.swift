14a38a4d17346e13f8cf7285a93e3f5aec33e26c
cirtuare <cirtuare@gmail.com>
2024-10-05 14:46:27 +0900

    [Chore] 주석 삭제

--- AND-SOPT-LeeSumin/SceneDelegate.swift
+++ AND-SOPT-LeeSumin/SceneDelegate.swift
@@ -15,13 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        // 2.
         self.window = UIWindow(windowScene: windowScene)
-        // 3.
         let navigationController = UINavigationController(rootViewController: ViewController())
-        navigationController.navigationBar.isHidden = true
         self.window?.rootViewController = navigationController
-        // 4.
         self.window?.makeKeyAndVisible()
     }
 
