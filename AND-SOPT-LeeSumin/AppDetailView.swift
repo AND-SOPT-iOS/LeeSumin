19944257d9ba713dfe178f81fd206eed86af83ec
cirtuare <cirtuare@gmail.com>
2024-10-05 15:20:21 +0900

    [Feat] AppDetailView 구조 세팅

--- AND-SOPT-LeeSumin/AppDetailView.swift
+++ AND-SOPT-LeeSumin/AppDetailView.swift
@@ -11,17 +11,52 @@ import Then
 
 class AppDetailView: UIView {
 
+    private var topView: UIView = UIView()
+    
+    private var appIcon : UIImageView = UIImageView()
+    
+    private var appTitle : UILabel = UILabel()
+    
+    private var appDescription : UILabel = UILabel()
+    
+    private var appInstallButton : UIButton = UIButton()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setHierarchy()
+        setLayout()
+        setStyle()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
+    private func setHierarchy() {
 
+    }
+
+    private func setLayout() {
+        appIcon.snp.makeConstraints {
+            
+        }
+        
+        appTitle.snp.makeConstraints {
+            
+        }
+        
+        appDescription.snp.makeConstraints {
+            
+        }
+        
+        appInstallButton.snp.makeConstraints {
+            
+        }
+    }
     
+    private func setStyle() {
+        
+    }
 
     
 }
