8d2758a42a04d9715c098e6b28285e1c7378b4a7
cirtuare <cirtuare@gmail.com>
2024-10-05 13:12:15 +0900

    Initial Commit

---
+++ AND-SOPT-LeeSuminTests/AND_SOPT_LeeSuminTests.swift
@@ -0,0 +1,17 @@
+//
+//  AND_SOPT_LeeSuminTests.swift
+//  AND-SOPT-LeeSuminTests
+//
+//  Created by 이수민 on 10/5/24.
+//
+
+import Testing
+@testable import AND_SOPT_LeeSumin
+
+struct AND_SOPT_LeeSuminTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
