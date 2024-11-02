dabf821019bac38716f562429ea2be4c69443081
cirtuare <cirtuare@gmail.com>
2024-11-02 15:10:25 +0900

    [Feat] BaseURL & Config 파일 세팅

---
+++ AND-SOPT-LeeSumin/Global/Configurartions/Environment.swift
@@ -0,0 +1,12 @@
+//
+//  Environmet.swift
+//  AND-SOPT-LeeSumin
+//
+//  Created by 이수민 on 11/2/24.
+//
+
+import Foundation
+
+enum Environment {
+  static let baseURL: String = Bundle.main.infoDictionary?["BASE_URL"] as! String
+}
